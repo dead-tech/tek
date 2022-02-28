927545be3ce773b4e5302a6fbe72dc667b8f3010
dead-tech <dev.dead.tech@gmail.com>
2022-02-28 14:07:35 +0100

    :sparkles: Improve run_test.py script with colors

--- run_tests.py
+++ run_tests.py
@@ -7,33 +7,57 @@ import sys
 BUILD_PATH: str = f'{os.getcwd()}/build/'
 BINARY_PATH: str = f'{os.getcwd()}/build/tek'
 TARGET: str = 'tek'
-EXAMPLES_PATH = f'{os.getcwd()}/tests/'
+TESTS_PATH = f'{os.getcwd()}/tests/'
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def color_red(msg: str) -> str:
+    return f'{bcolors.FAIL}{msg}{bcolors.ENDC}'
+
+
+def color_green(msg: str) -> str:
+    return f'{bcolors.OKGREEN}{msg}{bcolors.ENDC}'
+
+
+def color_header(msg: str) -> str:
+    return f'{bcolors.HEADER}{msg}{bcolors.ENDC}'
 
 
 def check_exit_code(result: subprocess.CompletedProcess, message: str) -> None:
     if result.returncode != 0:
-        print(f'[ERROR] {message}')
+        print(color_red(f'[ERROR] {message}'))
         sys.exit(1)
 
 
 def build_executable() -> None:
     if not os.getcwd().endswith('/build'):
         os.chdir(BUILD_PATH)
-    print(f'[INFO] Generating cmake project in {BUILD_PATH}...')
-    result = subprocess.run(['cmake', '..'])
+
+    result = subprocess.run(['cmake', '..'], capture_output=True)
     check_exit_code(result, 'Unable to generate cmake project...')
-    print(f'[INFO] Building target {TARGET} in {BUILD_PATH}...')
-    result = subprocess.run(['make', 'tek'])
+
+    result = subprocess.run(['make', 'tek'], capture_output=True)
     check_exit_code(result, f'Unable to build target {TARGET}...')
-    print(f'[INFO] Built target {TARGET}...')
+
+    print(color_green(f'[BUILD] Built target {TARGET}'))
 
 
 def find_examples() -> list[str]:
     out: list[str] = []
-    print(f'[INFO] Searching examples to run in {EXAMPLES_PATH}')
-    for dirpath, _, filenames in os.walk(EXAMPLES_PATH):
+
+    for dirpath, _, filenames in os.walk(TESTS_PATH):
         for filename in [f for f in filenames if f.endswith('.tek')]:
-            print(f'Found {filename}')
             out.append(os.path.join(dirpath, filename))
 
     return out
@@ -43,8 +67,27 @@ def print_results(
     actual_result: subprocess.CompletedProcess,
     expected_result: str,
 ) -> None:
-    print(f'[NOTE] Got:      {actual_result.stdout}')
-    print(f'[NOTE] Expected: {expected_result}')
+    print(f'{color_red(f"[NOTE] Got:      {actual_result.stdout}")}')
+    print(f'{color_red(f"[NOTE] Expected: {expected_result}")}')
+
+
+def print_succeeding_test(filename: str) -> None:
+    left_column = f'[TEST] {filename}'
+    print(color_green(left_column + 'SUCCESS'.rjust(80 - len(left_column), '.')))  # noqa: E501
+
+
+def print_failing_test(filename: str) -> None:
+    left_column = f'[TEST] {filename}'
+    print(color_red(left_column + 'FAILED'.rjust(80 - len(left_column), '.')))
+
+
+def assert_results(filename, assert_expression: bool) -> bool:
+    try:
+        assert(assert_expression)
+        print_succeeding_test(filename)
+        return True
+    except AssertionError:
+        print_failing_test(filename)
 
 
 def run_examples(examples: list[str]) -> None:
@@ -52,10 +95,8 @@ def run_examples(examples: list[str]) -> None:
         os.chdir(BUILD_PATH)
 
     succeeding = 0
-    failing = 0
-    skipped = 0
+    ignored = 0
 
-    print('[INFO] Running tests')
     for example in examples:
         filename = example.split('/')[-1]
         with open(example) as file:
@@ -63,7 +104,7 @@ def run_examples(examples: list[str]) -> None:
             first_line: str = lines[0]
 
             if 'ignore' in first_line.lower():
-                skipped += 1
+                ignored += 1
                 continue
 
             last_line: str = lines[-1]
@@ -75,29 +116,20 @@ def run_examples(examples: list[str]) -> None:
             result = subprocess.run(['./tek', example], capture_output=True)
 
             if expected_result == '(fail)':
-                try:
-                    assert(result.returncode != 0)
+                if assert_results(filename, result.returncode != 0):
                     succeeding += 1
-                    print(f'[TEST] {filename}...SUCCESS')
-                except AssertionError:
-                    failing += 1
-                    print(f'[ERROR] Test {filename} FAILED')
-                    print(f'[INFO] Test was supposed to fail but exit code was {result.returncode}')  # noqa: E501
+                else:
                     print_results(result, expected_result)
                     sys.exit(1)
                 continue
 
-            try:
-                assert(expected_result == result.stdout.decode())
+            if assert_results(filename, expected_result == result.stdout.decode()):  # noqa: E501
                 succeeding += 1
-                print(f'[TEST] {filename}...SUCCESS')
-            except AssertionError:
-                failing += 1
-                print(f'[ERROR] Test {filename} FAILED')
+            else:
                 print_results(result, expected_result)
                 sys.exit(1)
 
-    print(f'\n[TESTS RECAP] succeeding: {succeeding}, failing: {failing}, skipped: {skipped}\n')  # noqa: E501
+    print(f'\n{color_header("[TESTS RECAP]")} {color_green(f"succeeding: {succeeding}")}, ignored: {ignored}\n')  # noqa: E501
 
 
 def main() -> int:
